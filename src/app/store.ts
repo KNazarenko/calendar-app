b1ab52adf98f66b470d080ce5002bab19c02c030
Nazarenko <nazarenko@MacBook-Pro.local>
2025-02-03 19:05:11 +0200

    refactoring remove unused

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,16 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit"
 import { combineSlices, configureStore } from "@reduxjs/toolkit"
 import { setupListeners } from "@reduxjs/toolkit/query"
-import { counterSlice } from "../features/counter/counterSlice"
-import { quotesApiSlice } from "../features/quotes/quotesApiSlice"
+import { publicHolidayApiSlice } from "../features/countries/publicHolidayApiSlice"
 
-const rootReducer = combineSlices(counterSlice, quotesApiSlice)
+const rootReducer = combineSlices(publicHolidayApiSlice)
 export type RootState = ReturnType<typeof rootReducer>
 
 export const makeStore = (preloadedState?: Partial<RootState>) => {
   const store = configureStore({
     reducer: rootReducer,
     middleware: getDefaultMiddleware => {
-      return getDefaultMiddleware().concat(quotesApiSlice.middleware)
+      return getDefaultMiddleware().concat(publicHolidayApiSlice.middleware)
     },
     preloadedState,
   })
